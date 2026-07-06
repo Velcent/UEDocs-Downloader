e99cccb764067ea0bbd87b55251d6a0219644fbc
Velcent <miftachul.huda.makruf@gmail.com>
2026-07-06 18:19:02 +0700

    Add : EOS Patcher

---
+++ MHTML-Patcher-EOS.ps1
@@ -0,0 +1,13 @@
+Get-ChildItem -LiteralPath .\mhtml -Filter *.mhtml -Recurse -File | ForEach-Object {
+    $path = $_.FullName
+    $content = Get-Content -LiteralPath $path -Raw -ErrorAction Stop
+    $content = $content -replace "=`r`n(?!`r`n)", ""
+    $content = $content -replace "<!---->", ""
+    $content = $content -replace "<iframe src", "<iframe allowfullscreen src"
+    $content = $content -replace '
+    (?sx)
+    <aside.*?</aside>
+    |<eos-navigation.*?</eos-navigation>
+    |<epicgames-footer.*?</epicgames-footer>', ''
+    Set-Content -LiteralPath $path -Value $content -ErrorAction Stop
+}
